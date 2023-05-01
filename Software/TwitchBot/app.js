5960ba8aa246bb83e8bb42cad5f1bf8f9c72345e
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2023-05-01 22:02:05 +0200

    feat: :sparkles: mod & admin allows to display url, stack message when in treatment

--- Software/TwitchBot/app.js
+++ Software/TwitchBot/app.js
@@ -20,60 +20,74 @@ const canvas = createCanvas(80, 72);
 const CM = new CanvasManager(canvas);
 const CV = new ConvertVideotex();
 
-function sliceIntoChunks(arr, chunkSize) {
+const sliceIntoChunks = (arr, chunkSize) => {
   const res = [];
   for (let i = 0; i < arr.length; i += chunkSize) {
     const chunk = arr.slice(i, i + chunkSize);
     res.push(chunk);
   }
   return res;
-}
+};
+
+const treatImage = async (img) => {
+  inTreatment = true;
+  const url = `http://${process.env.MINITEL_IP}`;
+  const chunks = sliceIntoChunks(CV.convert(CM.getPixels(img)), 100);
+  await axios.get(`${url}/new`);
+  for (let i = 0; i < chunks.length; i++) {
+    await axios.post(
+      `${url}/post`,
+      {
+        encrypt: Uint8Array.from(chunks[i]),
+      },
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+  }
+  await axios.get(`${url}/end`);
+  inTreatment = false;
+  setTimeout(() => {
+    if (messageStack.length) {
+      treatMessage(messageStack.shift());
+    }
+  }, 2000); // we display at least 2 sec the image
+};
+
+const treatMessage = ({ message, tags }) => {
+  const parts = message.toString().split(" ");
+  const cmd = parts.shift();
+  if (cmd.toLowerCase() === "!3615") {
+    const prompt = parts.join(" ");
+    if ((tags.mod || tags.badges.broadcaster) && prompt.startsWith("http")) {
+      // is moderator, then this viewer can send image directly
+      loadImage(prompt).then(treatImage);
+    } else {
+      axios
+        .get(
+          `https://api.giphy.com/v1/gifs/random?api_key=${
+            process.env.GIPHY_API
+          }&tag=${encodeURI(prompt)}`
+        )
+        .then((response) => {
+          loadImage(response.data.data.images.original.url).then(treatImage);
+        });
+    }
+  }
+};
+
+const messageStack = [];
+let inTreatment = false;
 
 client.connect().catch(console.error);
 client.on("message", (channel, tags, message, self) => {
   if (self) return;
-  switch (message.toLowerCase()) {
-    default:
-      const parts = message.toString().split(" ");
-      const cmd = parts.shift();
-      if (cmd.toLowerCase() === "!3615") {
-        const prompt = parts.join(" ");
-        const url = `http://${process.env.MINITEL_IP}`;
-        axios
-          .get(
-            `https://api.giphy.com/v1/gifs/random?api_key=${
-              process.env.GIPHY_API
-            }&tag=${encodeURI(prompt)}`
-          )
-          .then((response) => {
-            loadImage(response.data.data.images.original.url).then(
-              async (img) => {
-                const chunks = sliceIntoChunks(
-                  CV.convert(CM.getPixels(img)),
-                  100
-                );
-                await axios.get(`${url}/new`);
-                for (let i = 0; i < chunks.length; i++) {
-                  await axios.post(
-                    `${url}/post`,
-                    {
-                      encrypt: Uint8Array.from(chunks[i]),
-                    },
-                    {
-                      headers: {
-                        "Content-Type": "multipart/form-data",
-                      },
-                    }
-                  );
-                }
-                await axios.get(`${url}/end`);
-              }
-            );
-          });
-        /*
-        
-        */
-      }
-      break;
+  if (inTreatment) {
+    messageStack.push({ message, tags });
+    return;
+  } else {
+    treatMessage({ message, tags });
   }
 });
